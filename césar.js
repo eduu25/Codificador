f7a728dee413141f624e06f16fffb85238b7b1e9
eduu25 <100290358+eduu25@users.noreply.github.com>
2022-04-07 19:00:27 -0300

    desenvolvimento da lógica cifraCésar

---
+++ césar.js
@@ -0,0 +1,25 @@
+function cifraCésar(str,num) {
+num = num % 26
+ var lowerCaseStr = str.toLowerCase();
+ var alfabeto = 'abcdefghijklmnopqrstuvwxyz'.split('');
+ var newStr = '';
+
+ for (var i = 0; i < lowerCaseStr.length; i++){
+     var letra = lowerCaseStr[i];
+     if(letra === ''){
+         newStr += letra
+        continue;
+     }
+    var letraIndex = alfabeto.indexOf(letra);
+    var newIndex = letraIndex + num;
+    if (newIndex > 25) newIndex = newIndex - 26
+    if (newIndex < 0) newIndex = newIndex + 26
+    if (str[i] === str[i].toUpperCase()){
+        newStr += alfabeto[newIndex].toUpperCase();
+    }
+  else newStr += alfabeto[newIndex]
+ }
+return newStr
+}
+console.log(cifraCésar("Texte", 5000))
+
