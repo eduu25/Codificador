0211ba8defd8c9f133f40276cabef4357660c224
eduu25 <100290358+eduu25@users.noreply.github.com>
2022-04-07 19:03:19 -0300

    js do codificador
    Base64, funcionamento dos botões+ lógica, evento click & preventDefault no form

---
+++ codificador.js
@@ -0,0 +1,24 @@
+var codificar = document.querySelector("#btcodificar")
+var decodificar = document.querySelector("#btdecodificar")
+var output = document.querySelector("#Texto")
+var formulario = document.querySelector("#formzz")
+
+function eventoForm(Event) {
+    Event.preventDefault()
+    
+}
+
+formulario.addEventListener("submit", eventoForm)
+
+codificar.addEventListener("click",  () => {
+    output.value = btoa(output.value);
+    
+  
+})
+
+decodificar.addEventListener("click",  () => {
+    output.value = atob(output.value);
+    
+
+  
+})
